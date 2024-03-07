5c32c9346af7abf5c4206a2864479ab0b7b3fb5c
GalievDev <ggvalion@gmail.com>
2024-03-07 15:10:11 +0300

    Initial commit

---
+++ src/main/java/dev/galiev/vkinternship/VkinternshipApplication.java
@@ -0,0 +1,13 @@
+package dev.galiev.vkinternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkinternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkinternshipApplication.class, args);
+	}
+
+}
