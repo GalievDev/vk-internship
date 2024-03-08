d848842688d5bc4a819e0c5fbf2e2c2a34e2608e
GalievDev <ggvalion@gmail.com>
2024-03-08 12:56:54 +0300

    Security settings

--- src/main/java/dev/galiev/vkinternship/VkinternshipApplication.java
+++ src/main/java/dev/galiev/vkinternship/VkinternshipApplication.java
@@ -2,9 +2,17 @@ package dev.galiev.vkinternship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @SpringBootApplication
 public class VkinternshipApplication {
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring()
+				.anyRequest();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VkinternshipApplication.class, args);
